62134f2c0a011d5bf2845b5f1bc463751fbfa338
abiard <abiard@btslivh.eu>
2021-01-05 17:26:20 +0100

    Mon main c++ est modifié

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
 int main(int argc, char** argv) {
     
     cout << "Bonjour" << endl;
+    cout << "La France" << endl;
     return 0;
 }
 

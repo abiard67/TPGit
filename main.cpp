94c4c3366c46b6e4946baa8189f5e3c41c6591d5
abiard <abiard@btslivh.eu>
2021-01-07 15:32:57 +0100

    Version 1

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(int argc, char** argv) {
 
     cout << "Bonjour" << endl;
-    cout << "Arno casse tout" << endl;
+    cout << "Arno: message 1" << endl;
 
     return 0;
 }

4b9edf8c2a7c863b016ffd7441613da4a1456580
mfournigault <mfournigault@btslivh.eu>
2021-01-19 17:22:32 +0100

    Modification d'un dépot distant

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char** argv) {
     cout << "Bonjour" << endl;
     cout << "Arno: message 1" << endl;
     cout << "Matthéo : réponse 1" << endl;
+    cout << "Matthéo : Ce que vous voulez..." << endl;
 
     return 0;
 }
